be3dd3035dcea3dce5e5692ff445bb61542b5de4
ZakKamila <82116408+ZakKamila@users.noreply.github.com>
2021-10-29 23:20:21 +0300

    Add files via upload

---
+++ Туристическое агенство/Туристическое агенство/Hotels.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Туристическое_агенство
+{
+    /// <summary>
+    /// Логика взаимодействия для Hotels.xaml
+    /// </summary>
+    public partial class Hotels : Page
+    {
+        public Hotels()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AppEdit());
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
